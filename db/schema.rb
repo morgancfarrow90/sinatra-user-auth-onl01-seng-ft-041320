5f96d15f9d82c320ec151e57919b1ba0ae2cc4b3
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-27 20:22:00 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200527192818) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
